a66f696292a3ec29d189de01a56c490df442fb0e
mustafa-Mlab <mustafa.kamal@monstar-lab.com>
2022-01-24 10:48:25 +0600

    feature 2 commit

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
-echo "Hello world";
-echo "Hello Mustafa";
-echo "hello Stranger";
+echo "Hello world"; // this is initial commit
+echo "Hello Mustafa"; // this is develop branch commit
+echo "hello Stranger"; // this is feature 1 commit
+echo "Feature 2"; // this is feature 2 commit 
